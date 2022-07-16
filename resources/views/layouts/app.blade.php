ae55c0792055908e7b96a5a3b6da270a59bc579e
eodero <emmanuelodero808@gmail.com>
2022-07-16 12:49:11 +0300

    added the bootstrap link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,12 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" 
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- configuring vite -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
 </head>
 <body>
     <div id="app">
