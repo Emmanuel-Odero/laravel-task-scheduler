f476908cae35e9bbc9ae75d9af606518ca1c1f9b
eodero <emmanuelodero808@gmail.com>
2022-07-16 18:36:29 +0300

    created routes for the create page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
-
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TasksController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,4 +22,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
-Route::resource('tasks', 'TasksController');
+Route::resource('/tasks', 'TasksController');
